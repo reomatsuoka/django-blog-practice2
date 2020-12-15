9132fba03b6924e2cbd19f5751fc84ff01520f3b
reomatsuoka <reo199637458@gmail.com>
2020-12-15 10:52:20 +0900

    新機能

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, verbose_name="カテゴリ", on_delete=models.PROTECT)
     title = models.CharField("タイトル", max_length=50)
     image = models.ImageField(upload_to='images', verbose_name='イメージ画像', null=True, blank=True)
     content = models.TextField("本文")
